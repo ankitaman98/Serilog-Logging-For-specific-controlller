3cd435a096e18f26fa62842437433fad2ab002ee
ANKIT AMAN <31533957+ankitaman98@users.noreply.github.com>
2022-06-19 22:26:44 +0530

    Add files via upload

---
+++ Example/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("STRING")]
+        public string Get()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"HOME CONTROLLER");
+            sb.AppendLine($"cALLED");
+            _logger.LogInformation(sb.ToString());
+            return "";
+
+        }
+
+    }
+}
---
+++ Example/Controllers/HomeController1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Text;
+
+namespace Example.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController1 : Controller
+    {
+        private readonly ILogger<HomeController1> _logger;
+
+        public HomeController1(ILogger<HomeController1> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("STRING11")]
+        public string Get()
+        {
+            return "";
+
+        }
+
+    }
+}
